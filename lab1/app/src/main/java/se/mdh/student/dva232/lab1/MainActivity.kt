b66bfe69657a92e10ce97f269c9cf609b7a8eba8
HarlockOfficial <harlockofficial.github@gmail.com>
2020-11-13 13:16:02 +0100

    Initial commit

---
+++ lab1/app/src/main/java/se/mdh/student/dva232/lab1/MainActivity.kt
@@ -0,0 +1,11 @@
+package se.mdh.student.dva232.lab1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
