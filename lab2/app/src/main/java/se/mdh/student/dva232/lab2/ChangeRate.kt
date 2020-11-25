f9d8dc3a0876e34abfcad268170663319eb3798d
HarlockOfficial <harlockofficial.github@gmail.com>
2020-11-25 22:19:58 +0100

    fixed Lab2

--- lab2/app/src/main/java/se/mdh/student/dva232/lab2/ChangeRate.kt
+++ lab2/app/src/main/java/se/mdh/student/dva232/lab2/ChangeRate.kt
@@ -2,6 +2,8 @@ package se.mdh.student.dva232.lab2
 
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import org.json.JSONException
 import org.json.JSONObject
@@ -16,20 +18,23 @@ class ChangeRate: ViewModel(){
             try {
                 val tmp = JSONObject(URL("https://api.exchangeratesapi.io/latest").readText())
                 for(key:String in tmp.getJSONObject("rates").keys()){
-                    currencyExchangeRates[key] = tmp.getJSONObject("rates").getDouble(key)
+                    currencyExchangeRates[key.trim()] = tmp.getJSONObject("rates").getDouble(key)
                 }
                 currencyExchangeRates["EUR"]=1.0
             }catch(unused: JSONException){}
         }
         fun getCurrencyList(): MutableSet<String>? {
-            return if(currencyExchangeRates.keys.size>0) currencyExchangeRates.keys else null
+            return if(currencyExchangeRates.keys.size>0) currencyExchangeRates.keys.toSortedSet() else null
         }
         fun getExchange(start: String, end: String): Double {
             if(currencyExchangeRates.isEmpty()){
+                GlobalScope.launch (Dispatchers.IO){
+                    updateExchangeRates()
+                }
                 return 0.0
             }
-            val change1:Double = currencyExchangeRates[start] ?: return 0.0
-            val change2:Double = currencyExchangeRates[end] ?: return 0.0   //always null for no reason
+            val change1:Double = currencyExchangeRates[start.trim()] ?: return 0.0
+            val change2:Double = currencyExchangeRates[end.trim()] ?: return 0.0
             return change2/change1
         }
     }
--- lab2/app/src/main/java/se/mdh/student/dva232/lab2/MainActivity.kt
+++ lab2/app/src/main/java/se/mdh/student/dva232/lab2/MainActivity.kt
@@ -1,33 +1,45 @@
 package se.mdh.student.dva232.lab2
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Geocoder
+import android.location.LocationManager
 import android.os.Bundle
 import android.widget.*
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     val inputCurrency = ItemSelectionEvent(input = true, mainActivity = this)
     val outputCurrency = ItemSelectionEvent(input = false, mainActivity = this)
-
     lateinit var output: TextView
         private set
     lateinit var input: EditText
         private set
+    private var useGeolocation : Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //ask for permissions
+        check_permissions()
+
         input = findViewById(R.id.user_input)
         output = findViewById(R.id.output)
 
-        GlobalScope.launch {
+        GlobalScope.launch(Dispatchers.IO) {
             ChangeRate.updateExchangeRates()
             var tmp : MutableList<String>? = ChangeRate.getCurrencyList()?.toMutableList()
-            while (tmp==null){
+            while (tmp==null) {
                 delay(10)
                 tmp = ChangeRate.getCurrencyList()?.toMutableList()
             }
@@ -45,8 +57,35 @@ class MainActivity : AppCompatActivity() {
                 }
                 spinner.onItemSelectedListener = inputCurrency
 
-                spinner = findViewById(R.id.dropdown_output)
+                if (ActivityCompat.checkSelfPermission(this@MainActivity,Manifest.permission.ACCESS_FINE_LOCATION
+                    ) == PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(this@MainActivity,
+                        Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    val locationManager: LocationManager =
+                        getSystemService(LOCATION_SERVICE) as LocationManager
+                    var provider: String = LocationManager.NETWORK_PROVIDER
+                    if (useGeolocation) {
+                        provider = LocationManager.GPS_PROVIDER
+                    }
+                    val location = locationManager.getLastKnownLocation(provider)
+                    val geoCoder = Geocoder(this@MainActivity)
+                    if (location != null) {
+                        //TODO buggy need to ask (does not work on virtual device)
+                        val actCurrency = Currency.getInstance(
+                            geoCoder.getFromLocation(
+                                location.latitude, location.longitude,
+                                1
+                            )[0].locale
+                        ).currencyCode
+                        for (i in 0 until spinner.count) {
+                            if (spinner.getItemAtPosition(i).toString() == actCurrency) {
+                                spinner.setSelection(i)
+                                break
+                            }
+                        }
+                    }
+                }
 
+                spinner = findViewById(R.id.dropdown_output)
                 ArrayAdapter(
                     this@MainActivity,
                     android.R.layout.simple_spinner_item,
@@ -66,4 +105,29 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
+    private fun check_permissions(){
+        val requestPermissionLauncher =
+            registerForActivityResult(
+                ActivityResultContracts.RequestPermission()
+            ) { isGranted: Boolean ->
+                if (!isGranted) {
+                    useGeolocation = true
+                }else{
+                    Toast.makeText(this, getString(R.string.location_disabled), Toast.LENGTH_SHORT).show()
+                }
+            }
+        requestPermissionLauncher.launch(Manifest.permission.ACCESS_COARSE_LOCATION)
+        requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED &&
+            ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED){
+            useGeolocation = true
+        }
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q) {
+            requestPermissionLauncher.launch(Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+        }
+    }
+
 }
